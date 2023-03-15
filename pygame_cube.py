67f6eccc352a2bea9351caa817e3fa87e930465d
Павел Кабенюк <pavelkabenuk@Pavels-MBP.Dlink>
2023-03-16 00:24:50 +0700

    Added other symbols

--- pygame_cube.py
+++ pygame_cube.py
@@ -1,14 +1,15 @@
 import pygame
 from math import *
 import numpy as np
+import random
 
 WIDTH = 800
 HEIGHT = 600
 BACKGROUND_COLOR = (64, 64, 64)
 SYMBOLS_COLOR = (255, 255, 255)
 
-scale = 100
-circle_pos = [WIDTH/2, HEIGHT/2]
+scale = 750
+circle_pos = [WIDTH / 2, HEIGHT / 2]
 angle = 0
 
 pygame.init()
@@ -16,20 +17,21 @@ pygame.init()
 pygame.display.set_caption("3D Cube")
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# Math and Matrix
-points = [np.matrix([-1, -1, 1]),
-          np.matrix([1, -1, 1]),
-          np.matrix([1, 1, 1]),
-          np.matrix([-1, 1, 1]),
-          np.matrix([-1, -1, -1]),
-          np.matrix([1, -1, -1]),
-          np.matrix([1, 1, -1]),
-          np.matrix([-1, 1, -1])]
-
 projection_matrix = np.matrix([
     [1, 0, 0],
     [0, 1, 0]
 ])
+
+
+def creatingSurface():
+    points = []
+
+    for z in range(-2, 2):
+        for y in range(-2, 2):
+            for x in range(-2, 2):
+                points.append(np.matrix([x / 10, y / 10, z / 10]))
+
+    return points
 
 
 # Martix rotation
@@ -61,25 +63,35 @@ def rotateZ(angle):
     return rotation
 
 
+points = creatingSurface()
+
 clock = pygame.time.Clock()
 while True:
 
     clock.tick(60)
     screen.fill(BACKGROUND_COLOR)
 
+    projected2d = []
+
+    counter = 0
     for point in points:
+        counter += 1
         rotated2d = np.dot(rotateZ(angle), point.reshape(3, 1))
         rotated2d = np.dot(rotateY(angle), rotated2d)
         rotated2d = np.dot(rotateX(angle), rotated2d)
 
-        projected2d = np.dot(projection_matrix, rotated2d)
+        projected2d.append(np.dot(projection_matrix, rotated2d))
 
-        x = int(projected2d[0][0] * scale) + circle_pos[0]
-        y = int(projected2d[1][0] * scale) + circle_pos[1]
+    for obj in projected2d:
+        x = int(obj[0][0] * scale) + circle_pos[0]
+        y = int(obj[1][0] * scale) + circle_pos[1]
 
         font = pygame.font.SysFont("symbol", 15)
         text = font.render("#", True, SYMBOLS_COLOR)
         screen.blit(text, (x, y))
+        """for yKoef in range(0, 8):
+            for xKoef in range(0, 8):
+                screen.blit(text, (x + xKoef * 10, y + yKoef * 10))"""
 
     angle += 0.01
 
